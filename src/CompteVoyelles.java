f04d725f0ae7ef70daca52d7484f1bb84b944f62
Nathanwack <nathan.wack1@gmail.com>
2025-12-09 10:10:05 +0100

    Dernier exercice fait, je dois juste refaire le 5

--- src/CompteVoyelles.java
+++ src/CompteVoyelles.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CompteVoyelles {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Veuillez entrer un texte : ");
+        String texte = scanner.nextLine();
+        String[] voyelles = {"a","e","i","o","u","y"};
+        int compteur = 0;
+        for (int i = 0; i< texte.length();i++){
+            char tocompare = texte.charAt(i);
+            for (String voyelle : voyelles) {
+                if (tocompare == (voyelle.charAt(0))) {
+                    compteur++;
+                }
+            }
+        }
+        System.out.print("Il y a dans ce texte : " + compteur +  " voyelles.");
+    }
 }
