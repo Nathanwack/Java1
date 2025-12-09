2d591435b728d995c521dd7afc28c79da898145e
Nathanwack <nathan.wack1@gmail.com>
2025-12-09 16:20:12 +0100

    Deux exercices en plus, deux encore à réaliser

---
+++ src/Filiere.java
@@ -0,0 +1,9 @@
+public class Filiere {
+    public String Code;
+    public String Libelle;
+
+    public Filiere(String Code, String Libelle) {
+        this.Code = Code;
+        this.Libelle = Libelle;
+    }
+}
