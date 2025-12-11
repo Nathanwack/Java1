e6fd16f1aeb661e1952af41c6f3bf9248d8924ee
Nathanwack <nathan.wack1@gmail.com>
2025-12-11 09:58:40 +0100

    l

--- src/Bibliothèque.java
+++ src/Bibliothèque.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import javax.print.attribute.standard.Media;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bibliothèque {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Livre> livres = new ArrayList<>();
+        switch (Menu(sc)) {
+            case "1" :
+                Livre NouveauLivre = new Livre(sc);
+                livres.add(NouveauLivre);
+            break;
+            case "2" :
+            break;
+        }
+    }
+    public static String Menu(Scanner sc){
+        System.out.println(
+                "Choisissez une option :\n" +
+                        "1. Créer un livre  " +
+                        "2. Supprimer un livre  " +
+                        "3. Voir les livres " +
+                        "4. Sortir du programme "
+        );
+        String retour = sc.nextLine();
+        return retour;
+
+    }
 }
--- src/Livre.java
+++ src/Livre.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Livre {
+    private String nom;
+    private String auteur;
+    private Double prix;
+    Scanner sc = new Scanner(System.in);
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+    public Double getPrix() {
+        return prix;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+    public void setPrix(Double prix) {
+        this.prix = prix;
+    }
+    public Livre(Scanner sc) {
+        System.out.print("Nom : ");
+        nom = sc.nextLine();
+        this.nom = nom;
+        System.out.print("Auteur : ");
+        auteur = sc.nextLine();
+        this.auteur = auteur;
+        System.out.print("Prix : ");
+        prix = sc.nextDouble();
+        this.prix = prix;
+    }
+
 }
+
